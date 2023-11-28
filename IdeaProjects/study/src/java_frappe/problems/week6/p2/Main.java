8bb462ed1400a9a2649d5d41d1942cef3778cf2f
poqo <zxvm5962@naver.com>
2023-11-28 18:30:41 +0900

    내용 추가

--- IdeaProjects/study/src/java_frappe/problems/week6/p2/Main.java
+++ IdeaProjects/study/src/java_frappe/problems/week6/p2/Main.java
@@ -3,7 +3,7 @@ package java_frappe.problems.week6.p2;
 public class Main {
     public static void main(String[] args) {
         Person person1, person2;
-        person1 = new Person("허종구", new Date("January", 1, 2021));
+        person1 = new Person("표준성", new Date("January", 1, 2021));
         person2 = new Person(person1);
 
         System.out.println(person1);
@@ -15,7 +15,7 @@ public class Main {
 
 
         Person person3;
-        person3 = new Person("유동연", new Date("January", 1, 2021));
+        person3 = new Person("양병현", new Date("January", 1, 2021));
         System.out.println(person3);
         person3.getBorn().setDate("April", 1, 2021);
         System.out.println(person3);
